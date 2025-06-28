f69bd73c5005d83b78dcd370dbdbb6f348323c64
sambhavieerla <120093711+sambhavieerla@users.noreply.github.com>
2025-06-28 15:53:41 +0530

    Add files via upload

--- Data/lib-site-packages/jedi/third_party/dummy.pyi
+++ Data/lib-site-packages/jedi/third_party/dummy.pyi
@@ -1,3 +1,14 @@
-from django.core.mail.backends.base import BaseEmailBackend
+import string
+from typing import Any, Dict, List, Optional, Tuple, Union
 
-class EmailBackend(BaseEmailBackend): ...
+from django.http.request import HttpRequest
+
+from .base import BaseEngine
+
+class TemplateStrings(BaseEngine):
+    template_dirs: Tuple[str]
+    def __init__(self, params: Dict[str, Union[Dict[Any, Any], List[Any], bool, str]]) -> None: ...
+
+class Template(string.Template):
+    template: str
+    def render(self, context: Optional[Dict[str, str]] = ..., request: Optional[HttpRequest] = ...) -> str: ...
